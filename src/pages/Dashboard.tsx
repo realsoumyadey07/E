e5cfe4351d3713076cde41eef268740fdce6a355
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-02-28 13:51:15 +0530

    widgetcontainer section is implemented

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,6 +3,7 @@ import AdminSidebar from "../components/AdminSidebar";
 import { BsSearch } from "react-icons/bs";
 
 import { FaUser } from "react-icons/fa6";
+import { HiTrendingUp } from "react-icons/hi";
 
 export default function Dashboard() {
   return (
@@ -18,7 +19,60 @@ export default function Dashboard() {
           <FaRegBell/>
           <FaUser/>
         </div>
+        <section className="widgetcontainer">
+          <WidgetItem 
+          percent={40} 
+          amount={true}
+          value={3400000}
+          heading="Revenue"
+          color="rgb(0,115,255)"/>
+          <WidgetItem 
+          percent={40} 
+          amount={true}
+          value={3400000}
+          heading="Revenue"
+          color="rgb(0,115,255)"/>
+          <WidgetItem 
+          percent={40} 
+          amount={true}
+          value={3400000}
+          heading="Revenue"
+          color="rgb(0,115,255)"/>
+          <WidgetItem 
+          percent={40} 
+          amount={true}
+          value={3400000}
+          heading="Revenue"
+          color="rgb(0,115,255)"/>
+        </section>
       </main>
     </div>
+  )
+}
+
+
+interface WidgetItemProps {
+  heading:string,
+  value: number,
+  percent: number,
+  color: string,
+  amount?: boolean
+}
+const WidgetItem = ({ 
+  heading, 
+  value, 
+  percent, 
+  color, 
+  amount }: WidgetItemProps)=>{
+  return (
+    <article className="widget">
+      <div className="widgetInfo">
+        <p>{heading}</p>
+        <h4>{amount? `${value}`: value}</h4>
+        {
+          percent>0? <span className="green"> <HiTrendingUp/> +{percent}%{" "} </span> : <span> <HiTrendingUp/> -{percent}%{" "} </span>
+        }
+      </div>
+    </article>
   )
 }
