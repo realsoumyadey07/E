d619af2eb5b1522d94bf8203a00dcf42b9ce229e
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-02-28 12:34:20 +0530

    searchbar has been added of the dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,8 @@
+import { FaRegBell } from "react-icons/fa";
 import AdminSidebar from "../components/AdminSidebar";
+import { BsSearch } from "react-icons/bs";
+
+import { FaUser } from "react-icons/fa6";
 
 export default function Dashboard() {
   return (
@@ -6,9 +10,13 @@ export default function Dashboard() {
       {/* sidebar */}
       <AdminSidebar/>
       {/* main */}
-      <main>
+      <main className="dashboard">
         <div className="bar">
-          
+          <BsSearch/>
+          <input 
+          type="text" placeholder="Search for data, users docs" />
+          <FaRegBell/>
+          <FaUser/>
         </div>
       </main>
     </div>
