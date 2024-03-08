1325f9529b6ade4c89215f108f07d2412e809b70
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-03-08 12:21:08 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Loader from "./components/Loader";
 
@@ -23,6 +23,7 @@ export default function App() {
 
       <Suspense fallback={<Loader/>}>
       <Routes>
+        <Route path="/" element={<Link to="/admin/dashboard"><button>Visit Dashboard</button></Link>}/>
         <Route path="/admin/dashboard" element={<Dashboard/>} />
         <Route path="/admin/products" element={<Products/>} />
         <Route path="/admin/transaction" element={<Transaction/>} />
